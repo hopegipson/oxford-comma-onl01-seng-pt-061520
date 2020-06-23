82a2d1f1d908901f7edba5c8aa0230a84e1c21ac
Hope Gipson <gipson.hope@gmail.com>
2020-06-23 23:59:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,13 @@
 def oxford_comma(array)
   if array.length == 2
-    array[2] << and
+    array[-2] << and
     array.join
-  end
-  if array.length >2
+  
+  elsif array.length >2
+    array[-2] << and
 array.join(", ")
+end
+else
+  
 array.last
 end
